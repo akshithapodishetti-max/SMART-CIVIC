0a8aa32dd1b77e447b21a8dc3730e6a944f723d7
akshithapodishetti-max <akshupodishetti@gmail.com>
2026-09-16 23:18:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template, request, jsonify
 from sentence_transformers import SentenceTransformer
 from transformers import pipeline
@@ -85,5 +84,9 @@ def classify():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    threading.Timer(
+        1,
+        lambda: webbrowser.open("http://127.0.0.1:5000")
+    ).start()
+
+ app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
